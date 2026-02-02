eeb8bc1ce2c4e625efb81c74e7ba79bb2a927c72
AgustinDimayuga <axocuadi@calpol.edu>
2026-02-02 07:33:41 -0800

    added toggle functionality with button

--- public/header.mjs
+++ public/header.mjs
@@ -1,24 +1,37 @@
 import { toHtmlElement } from "./toHtmlElement.mjs";
-const headerHtmlString = `
+
+function createHeader() {
+  const headerHtmlString = `
     <header>
-      <div class="name-links">
+      <div class="left-nav">
         <h1>Agustin Xocua Dimayuga</h1>
-        <nav>
-          <ul class="links">
-            <li class="test"><a href="index.html">Home</a></li>
-            <li><a href="hobbies.html">Hobbies</a></li>
-          </ul>
-        </nav>
+        <div class="links">
+          <nav>
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="hobbies.html">Hobbies</a></li>
+            </ul>
+          </nav>
+        </div>
       </div>
       <div class="right-nav">
         <label>
           <input type="checkbox" autocomplete="off" />
           Dark Mode
         </label>
-        <button class="button">test</button>
+        <button class="button">Menu</button>
       </div>
     </header>
 `;
-const headerHtml = toHtmlElement(headerHtmlString);
-const body = document.body;
-body.prepend(headerHtml);
+  const headerHtml = toHtmlElement(headerHtmlString);
+  const body = document.body;
+  body.prepend(headerHtml);
+  const header = body.querySelector("header");
+  const btn = header.querySelector("button");
+  const links = body.querySelector(".links");
+  btn.addEventListener("click", () => {
+    links.classList.toggle("links");
+  });
+}
+
+createHeader();
